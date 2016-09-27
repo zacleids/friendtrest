67bf2c8df803c4771915a9f184e42a4a3a85aa5f
code-ee <copatterson@csumb.edu>
2016-09-26 19:18:09 -0700

    added login files

---
+++ src/main/java/com/friendtrest/PattersonUser.java
@@ -0,0 +1,35 @@
+package com.friendtrest;
+import java.util.ArrayList;
+
+public class PattersonUser {
+	private String username;
+	private String password;
+
+	public PattersonUser(String username, String password){
+		this.username = username;
+		this.password = password;
+	}
+
+	public String getUsername() {
+		return new String(username);
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return new String(password);
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String toString() {
+		return (
+			"Username: " + getUsername() + "\n" +
+			"Password: " + getPassword() + "\n"
+		);
+	}
+}
