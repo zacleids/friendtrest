db5c268854e3b1164634eb5fe07b1f8e2db05acb
Austin Zielinski <austinzielinski@Austin-Zielinskis-MacBook-Pro.local>
2016-10-11 18:42:14 -0700

    commiting a lot of new files for search

---
+++ src/test/java/com/friendtrest/integration/HomeSearchIT.java
@@ -0,0 +1,35 @@
+package com.friendtrest.integration;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.junit.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/**
+ * Created by austinzielinski on 10/7/16.
+ */
+public class HomeSearchIT {
+
+    WebDriver driver = new ChromeDriver();
+
+    @Before
+    public void setUp() {
+        driver.get("http://localhost:8080/index.html");
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void testGetQuery() {
+        assertTrue(false);
+    }
+}
