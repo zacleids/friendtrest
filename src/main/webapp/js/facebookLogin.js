cd63065575a5f5822e8cb98f3e7ab936e507bdae
code-ee <copatterson@csumb.edu>
2016-10-27 10:58:04 -0700

    removed some comments on my JS file

--- src/main/webapp/js/facebookLogin.js
+++ src/main/webapp/js/facebookLogin.js
@@ -2,10 +2,6 @@
   function statusChangeCallback(response) {
       console.log('statusChangeCallback');
       console.log(response);
-      // The response object is returned with a status field that lets the
-      // app know the current login status of the person.
-      // Full docs on the response object can be found in the documentation
-      // for FB.getLoginStatus().
       if (response.status === 'connected') {
           // Logged into your app and Facebook.
           testAPI();
@@ -24,8 +20,7 @@
   }
 
   // This function is called when someone finishes with the Login
-  // Button.  See the onlogin handler attached to it in the sample
-  // code below.
+  // Button.
   function checkLoginState() {
       FB.getLoginStatus(function(response) {
           statusChangeCallback(response);
@@ -41,17 +36,6 @@
           version: 'v2.6' // use graph api version 2.5
       });
 
-      // Now that we've initialized the JavaScript SDK, we call 
-      // FB.getLoginStatus().  This function gets the state of the
-      // person visiting this page and can return one of three states to
-      // the callback you provide.  They can be:
-      //
-      // 1. Logged into your app ('connected')
-      // 2. Logged into Facebook, but not your app ('not_authorized')
-      // 3. Not logged into Facebook and can't tell if they are logged into
-      //    your app or not.
-      //
-      // These three cases are handled in the callback function.
 
       FB.getLoginStatus(function(response) {
           statusChangeCallback(response);
@@ -69,8 +53,6 @@
       fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));
 
-  // Here we run a very simple test of the Graph API after login is
-  // successful.  See statusChangeCallback() for when this call is made.
   function testAPI() {
       console.log('Welcome!  Fetching your information.... ');
       FB.api('/me', function(response) {
