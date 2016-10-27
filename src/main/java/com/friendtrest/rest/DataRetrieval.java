a119b0227f7356cb34cda279300d9110f0d8cfcf
ziel5122 <azielinski@csumb.edu>
2016-10-27 00:30:31 -0700

    made the frontpage actually have things
    Summary: pushing all of the backend that I worked on that apparently wasn't in the repo.
    
    Test Plan: Will implement integration tests for DB functionality
    
    Reviewers: zacleids, code-ee
    
    Subscribers:

--- src/main/java/com/friendtrest/rest/DataRetrieval.java
+++ src/main/java/com/friendtrest/rest/DataRetrieval.java
@@ -1,28 +1,29 @@
-package com.friendtrest.database;
+package com.friendtrest.rest;
 
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-
-import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.amazonaws.services.dynamodbv2.model.ScanRequest;
 import com.amazonaws.services.dynamodbv2.model.ScanResult;
+import com.friendtrest.database.DBController;
 
-import java.util.Map;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
 
 /**
  * Created by Austin on 10/26/2016.
  */
-public class ScanDemo {
+@Path("/")
+public class DataRetrieval {
 
-    public static void main(String[] args) {
+    @GET
+    @Path("/data")
+    public Response query() {
         DBController dbc = new DBController();
         AmazonDynamoDB client = dbc.getAmazonDynamoDB();
 
         ScanRequest scanRequest = new ScanRequest().withTableName("Items");
 
         ScanResult result = client.scan(scanRequest);
-
-        for (Map<String, AttributeValue> item : result.getItems()){
-            System.out.println(item);
-        }
+        return Response.ok("response hello").build();
     }
 }
