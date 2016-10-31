ce58ccfa8ecf7a4ad33789ac507b26e6df9fa2bf
Zac Leids <zleids@csumb.edu>
2016-10-30 19:24:05 -0700

    made simpleSearch endpoint

--- src/main/java/com/friendtrest/database/SaveDemo.java
+++ src/main/java/com/friendtrest/database/SaveDemo.java
@@ -3,6 +3,7 @@ package com.friendtrest.database;
 import com.friendtrest.data.Album;
 import com.friendtrest.data.Book;
 import com.friendtrest.data.Item;
+import com.friendtrest.data.Movie;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,8 +17,10 @@ public class SaveDemo {
         DBController dbc = new DBController();
 
         List<String> tags = new ArrayList<String>();
-        tags.add("Jazz");
-        tags.add("Cool");
+        tags.add("Action");
+        tags.add("Fighting");
+        tags.add("Romance");
+        tags.add("Hunger Games");
 
         Item book_example = new Book(
                 "Ender's Game",
@@ -53,8 +56,60 @@ public class SaveDemo {
             songs,
             "https://upload.wikimedia.org/wikipedia/en/e/e5/Time_out_album_cover.jpg"
         );
+        /*
+        In the ruins of a place once known as North America lies the nation of Panem,
+        a shining Capitol surrounded by twelve outlying districts. Long ago the districts
+        waged war on the Capitol and were defeated. As part of the surrender terms, each district
+        agreed to send one boy and one girl to appear in an annual televised event called, \"The Hunger Games,\"
+        a fight to the death on live TV. Sixteen-year-old Katniss Everdeen, who lives alone with her mother and
+        younger sister, regards it as a death sentence when she is forced to represent her district in the Games.
+        The terrain, rules, and level of audience participation may change but one thing is constant: kill or be killed
+         */
 
-        Item example = album_example;
+        Item book_hunger_games = new Book(
+                "The Hunger Games",
+                "In the ruins of a place once known as North America lies the nation of Panem, " +
+                        "a shining Capitol surrounded by twelve outlying districts. Long ago the districts " +
+                        "waged war on the Capitol and were defeated. As part of the surrender terms, each district " +
+                        "agreed to send one boy and one girl to appear in an annual televised event called, \"The Hunger Games,\" " +
+                        "a fight to the death on live TV. Sixteen-year-old Katniss Everdeen, who lives alone with her mother and " +
+                        "younger sister, regards it as a death sentence when she is forced to represent her district in the Games. " +
+                        "The terrain, rules, and level of audience participation may change but one thing is constant: kill or be killed.",
+                tags,
+                "2008",
+                "9780439023528",
+                "Suzanne Collins",
+                "https://pictures.abebooks.com/isbn/9780439023528-us.jpg"
+        );
+        ArrayList<String> cast = new ArrayList<String>();
+        ArrayList<String> t = new ArrayList<String>();
+        t.add("Action");
+        t.add("Fighting");
+        t.add("Romance");
+        t.add("Hunger Games");
+        cast.add("Jennifer Lawrence");
+        cast.add("Liam Hemsworth");
+        cast.add("Willow Shields");
+        cast.add("Josh Hutcherson");
+        Item movie_hunger_games = new Movie(
+                "The Hunger Games",
+                "In the ruins of a place once known as North America lies the nation of Panem, " +
+                        "a shining Capitol surrounded by twelve outlying districts. Long ago the districts " +
+                        "waged war on the Capitol and were defeated. As part of the surrender terms, each district " +
+                        "agreed to send one boy and one girl to appear in an annual televised event called, \"The Hunger Games,\" " +
+                        "a fight to the death on live TV. Sixteen-year-old Katniss Everdeen, who lives alone with her mother and " +
+                        "younger sister, regards it as a death sentence when she is forced to represent her district in the Games. " +
+                        "The terrain, rules, and level of audience participation may change but one thing is constant: kill or be killed.",
+                t,
+                "2012",
+                cast,
+                "Francis Lawrence ",
+                "PG-13",
+                142,
+                "https://pictures.abebooks.com/isbn/9780439023528-us.jpg"
+        );
+
+        Item example = movie_hunger_games;
         System.out.println(example.getUUID());
         Save.saveItem(example, dbc);
         Item load = (Item)Load.loadItem(example.getUUID(), dbc);
@@ -64,5 +119,10 @@ public class SaveDemo {
         }
         System.out.println(load.getName());
         System.out.println(load.getDescription());
+        Movie m = (Movie)load;
+        System.out.println(m.getRunTimeInMinutes());
+        System.out.println(m.getMPAARating());
+        //Book b = (Book)load;
+        //System.out.println(b.getISBN());
     }
 }
--- src/main/java/com/friendtrest/rest/DataRetrieval.java
+++ src/main/java/com/friendtrest/rest/DataRetrieval.java
@@ -1,6 +1,10 @@
 package com.friendtrest.rest;
 
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.datamodeling.PaginatedScanList;
+import com.amazonaws.services.dynamodbv2.model.ScanRequest;
+import com.amazonaws.services.dynamodbv2.model.ScanResult;
 import com.friendtrest.data.Item;
 import com.friendtrest.database.DBController;
 
@@ -9,31 +13,43 @@ import com.google.gson.Gson;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.ArrayList;
 
 /**
  * Created by Austin on 10/26/2016.
  */
 @Path("/")
 public class DataRetrieval {
+    DBController dbc = new DBController();
+    AmazonDynamoDB client = dbc.getAmazonDynamoDB();
 
     @GET
     @Path("/data")
     public Response query() {
-        DBController dbc = new DBController();
+        ScanRequest scanRequest = new ScanRequest().withTableName("Items");
 
-        PaginatedScanList<Item> items = Scan.getItemsTable(dbc);
-        String json = new Gson().toJson(items);
+        ScanResult result = client.scan(scanRequest);
+        String json = new Gson().toJson(result);
 
         return Response.ok(json, MediaType.APPLICATION_JSON).build();
     }
 
     @GET
     @Path("/simpleSearch")
-    public Response simpleSearch(){
-
-        return Response.ok("temp").build();
+    public Response simpleSearch(@QueryParam("name") String name){
+        PaginatedScanList<Item> items = Scan.getItemsTable(dbc);
+        ArrayList<Item> itemsToShow = new ArrayList<Item>();
+        for (Item item : items) {
+            //System.out.println(item.getName());
+            if(item.getName().contains(name)){
+                itemsToShow.add(item);
+            }
+        }
+        String json = new Gson().toJson(itemsToShow);
+        return Response.ok(json, MediaType.APPLICATION_JSON).build();
     }
 
     @GET
