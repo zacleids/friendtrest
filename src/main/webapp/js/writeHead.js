8334e5144110c12dd4431574ea2ab6ac6768dc58
code-ee <copatterson@csumb.edu>
2016-10-30 17:01:22 -0700

    cleaned up heads, added files

---
+++ src/main/webapp/js/writeHead.js
@@ -0,0 +1,24 @@
+document.write(
+    '<!-- information -->' + '\n' +
+    '<meta charset="utf-8">' + '\n' +
+    '<title>Friendtrest</title>' + '\n' +
+    '<meta name="description" content="Friendtrest">' + '\n' +
+    '<meta name="author" content="patt3273">' + '\n' +
+    '<meta name="viewport" content="width=device-width, initial-scale=1.0">' + '\n' +
+    '' + '\n' +
+    '<!-- ***** links to libraries and frameworks ***** -->' + '\n' +
+    '<!-- ajax -->' + '\n' +
+    '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>' + '\n' +
+    '<!-- Latest compiled and minified CSS -->' + '\n' +
+    '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">' + '\n' +
+    '<!-- Optional theme -->' + '\n' +
+    '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">' + '\n' +
+    '<!-- Latest compiled and minified JavaScript -->' + '\n' +
+    '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>' + '\n' +
+    '' + '\n' +
+    '<!-- links to local files -->' + '\n' +
+    '<script type="text/javascript" src="js/facebookLogin.js"></script>' + '\n' +
+    '<script type="text/javascript" src="js/appearance.js"></script>' + '\n' +
+    '<script type="text/javascript" src="js/array.js"></script>' + '\n' +
+    '<link rel="stylesheet" type="text/css" href="css/bootstrap-social/bootstrap-social.css">' + '\n'
+);
