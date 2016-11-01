5321b109dcdeab3d2effa3af9485bef79689f620
ziel5122 <azielinski@csumb.edu>
2016-11-01 00:51:20 -0700

    Diffing a lot of stuff. Catching up on code that mysteriously didn't make it to git. The Integration tests are ReviewIT and SaveIT. Also changed a lot of the front end rating code.
    Summary: SaveIT and ReviewIT are the ones you need to pay attention to. Did some refactoring to dynamically generate rating/review popups.
    
    Test Plan: Need to test and refactor a lot of the code I added.
    
    Reviewers: zacleids, code-ee
    
    Differential Revision: http://csumbse.net/D254

---
+++ src/main/java/com/friendtrest/data/Review.java
@@ -0,0 +1,40 @@
+package com.friendtrest.data;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+
+@DynamoDBTable(tableName = "ReviewTest")
+public class Review {
+
+    private double rating;
+
+    private String item_id;
+    private String review_text;
+    private String user_id;
+    private String uuid;
+
+    public Review() {}
+
+    public Review(String item_id, String user_id, double rating, String review_text) {
+        this.item_id = item_id;
+        this.user_id = user_id;
+        this.rating = rating;
+        this.review_text = review_text;
+    }
+
+    public String getItem_id() { return item_id; }
+    public void setItem_id(String item_id) { this.item_id = item_id; }
+
+    public double getRating() { return rating; }
+    public void setRating(double rating) { this.rating = rating; }
+
+    public String getReview_text() { return review_text; }
+    public void setReview_text(String review_text) { this.review_text = review_text; }
+
+    public String getUser_id() { return user_id; }
+    public void setUser_id(String user_id) { this.user_id = user_id; }
+
+    @DynamoDBHashKey
+    public String getUuid() { return uuid; }
+    public void setUuid(String uuid) { this.uuid = uuid; }
+}
