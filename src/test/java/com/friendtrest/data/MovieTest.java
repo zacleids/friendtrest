5dea94b540ea32e711c404e595f9bf970524b916
Zac Leids <zleids@csumb.edu>
2016-11-08 15:54:46 -0800

    added unit tests for classes that entend Item
    Test Plan: n/a
    
    Reviewers: code-ee, ziel5122
    
    Differential Revision: http://csumbse.net/D259

---
+++ src/test/java/com/friendtrest/data/MovieTest.java
@@ -0,0 +1,116 @@
+package com.friendtrest.data;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.*;
+
+/**
+ * Created by Zac Leids on 10/13/2016.
+ */
+public class MovieTest {
+    Movie m;
+    String title;
+    String description;
+    ArrayList<String> tags = new ArrayList<String>();
+    String releaseDate;
+    ArrayList<String> cast = new ArrayList<String>();
+    String director;
+    String ratting;
+    int runTime;
+    String url;
+
+    @Before
+    public void setUp(){
+        title = "Deadpool";
+        description = "Crazy person with powers";
+        tags.add("Action");
+        tags.add("Adventure");
+        tags.add("Comedy");
+        releaseDate = "2016";
+        cast.add("Ryan Reynolds");
+        cast.add("Ed Skrein");
+        cast.add("Karan Soni");
+        director = "Tim Miller";
+        ratting = "R";
+        runTime = 128;
+        url = "https://images-na.ssl-images-amazon.com/images/M/MV5BMjQyODg5Njc4N15BMl5BanBnXkFtZTgwMzExMjE3NzE@._V1_UY268_CR1,0,182,268_AL_.jpg";
+
+        m = new Movie(
+                title,
+                description,
+                tags,
+                releaseDate,
+                cast,
+                director,
+                ratting,
+                runTime,
+                url
+        );
+    }
+
+
+    @Test
+    public void testGetName(){
+        assertThat(m.getName(), equalTo(title));
+    }
+
+    @Test
+    public void testGetDescription(){
+        assertThat(m.getDescription(), equalTo(description));
+    }
+
+    @Test
+    public void testNumTags(){
+        assertTrue(m.getNumTags() == tags.size());
+    }
+
+    @Test
+    public void testGetReleaseDate(){
+        assertThat(m.getReleaseDate(), equalTo(releaseDate));
+    }
+
+    @Test
+    public void testGetURL(){
+        assertThat(m.getURL(), equalTo(url));
+    }
+
+    @Test
+    public void testCastExists(){
+        assertTrue(m.getActors().contains(cast.get(0)));
+    }
+
+    @Test
+    public void testGetDirector(){
+        assertThat(m.getDirector(), equalTo(director));
+    }
+
+    @Test
+    public void testGetRatting(){
+        assertThat(m.getMPAARating(), equalTo(ratting));
+    }
+
+    @Test
+    public void testGetRunTime(){
+        assertTrue(m.getRunTimeInMinutes() == runTime);
+    }
+
+    @Test
+    public void testAddTag(){
+        int n = m.getNumTags();
+        m.addTag("Test");
+        assertTrue(n+1 == m.getNumTags());
+    }
+
+    @Test
+    public void testRemoveTag(){
+        int n = m.getNumTags();
+        m.removeTag("Adventure");
+        assertTrue(n-1 == m.getNumTags());
+    }
+
+
+}
